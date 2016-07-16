6f467ea4e875899bfe30140af74a3ce781c946d3
tylderen <russell.guo@bosondata.com.cn>
2016-07-16 11:53:08 +0800

    refine Worker.run

--- fulmar/worker/worker.py
+++ fulmar/worker/worker.py
@@ -1,6 +1,5 @@
 # -*- encoding: utf-8 -*-
 from __future__ import unicode_literals
-
 import logging
 
 import tornado.httpclient
@@ -32,52 +31,43 @@ class Worker(object):
                                    user_agent=user_agent, ioloop=self.ioloop,
                                    newtask_queue=newtask_queue, projectdb=projectdb)
 
-        self._running = False
-        self._quit = False
-
     def run(self):
         """Run ioloop"""
         logger.info("worker starting...")
 
         def queue_loop():
-
             if not self.readytask_queue:
                 logger.error('No readytask_queue provided!')
 
-            while not self._quit:
-                try:
-                    if self.requestor.free_size() <= 0:
-                        logger.warning('Too many requests is running!')
-                        break
-                    task = self.readytask_queue.pop()
-                    if task:
-                        crawl_rate = task.get('crawl_rate')
+            try:
+                if self.requestor.free_size() <= 0:
+                    logger.warning('Too many requests is running!')
+                    return
+                task = self.readytask_queue.pop()
+                if task:
+                    crawl_rate = task.get('crawl_rate')
+                    if crawl_rate:
+                        key_name = crawl_rate.get('key_name')
+                        request_number = crawl_rate.get('request_number')
+                        time_period = crawl_rate.get('time_period')
+                        if lua_rate_limit(keys=[key_name], args=[time_period, request_number]):
+                            # Request too fast, push the task back to newtask_queue.
+                            self.requestor.add_follows(task)
+                    result = self.requestor.request(task)
+            except Exception as e:
+                logger.exception(e)
 
-                        if crawl_rate: # Crawl rate is setted.
-                            key_name = crawl_rate.get('key_name')
-                            request_number = crawl_rate.get('request_number')
-                            time_period = crawl_rate.get('time_period')
-                            if lua_rate_limit(keys=[key_name], args=[time_period, request_number]):
-                                # Request too fast, push the task back to newtask_queue.
-                                # push_job(tasks=[task])
-                                self.requestor.add_follows(task)
-                                break
-                        result = self.requestor.request(task)
-                    break
-                except Exception as e:
-                    logger.exception(e)
-                    break
-        tornado.ioloop.PeriodicCallback(queue_loop, 100, io_loop=self.ioloop).start()
-        self._running = True
+        tornado.ioloop.PeriodicCallback(queue_loop, 50, io_loop=self.ioloop).start()
+
         try:
             self.ioloop.start()
         except KeyboardInterrupt:
             logger.info('KeyboardInterrupt. Bye bye.')
+        finally:
+            self.stop()
 
         logger.info("worker exiting...")
 
-    def quit(self):
+    def stop(self):
         """Quit worker"""
-        self._running = False
-        self._quit = True
         self.ioloop.stop()
