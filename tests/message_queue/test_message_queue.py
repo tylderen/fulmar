2373aeadd797137996367b90144845dd554ac348
tylderen <russell.guo@bosondata.com.cn>
2016-07-29 15:06:46 +0800

    tests: add test case for cron_queue

--- tests/message_queue/test_message_queue.py
+++ tests/message_queue/test_message_queue.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from fulmar.message_queue.redis_queue  import NewTaskQueue, ReadyQueue
+from fulmar.message_queue.redis_queue  import NewTaskQueue, ReadyQueue, CronQueue
 
 
 def test_newtask_queue(redis_conn):
@@ -38,3 +38,23 @@ def test_ready_queue(redis_conn):
     assert ready_queue.get() == {'name': 'task_2', 'priority': 3}
     assert ready_queue.get() == {'name': 'task_1'}
     assert len(ready_queue) == 0
+
+
+def test_cron_queue(redis_conn):
+    cron_queue = CronQueue(redis_conn, 'test_cron_queue')
+    cron_queue.clear()
+    assert len(cron_queue) == 0
+
+    with pytest.raises(TypeError):
+        cron_queue.put('task_str')
+    with pytest.raises(TypeError):
+        cron_queue.put({'name': 'task_2', 'crawl_': '3'}, '100000')
+
+    cron_queue.put({'name': 'task_2', 'priority': 3, 'crawl_later': 3}, 100001)
+    cron_queue.put({'name': 'task_3', 'priority': 4, 'crawl_later': 4}, 100002)
+
+    assert len(cron_queue) == 2
+
+    assert cron_queue.get() == ({'name': 'task_2', 'priority': 3, 'crawl_later': 3}, 100001)
+    assert cron_queue.get() == ({'name': 'task_3', 'priority': 4, 'crawl_later': 4}, 100002)
+    assert len(cron_queue) == 0
